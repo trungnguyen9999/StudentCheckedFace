f913ecfbf2524591420e57e2d7e957ac5e40d32b
trungnguyen9999 <trungnguyen.prm@gmail.com>
2022-12-17 14:34:32 +0700

    side menu

--- gui/quantri.py
+++ gui/quantri.py
@@ -144,7 +144,7 @@ class Ui_MainWindow(object):
 
 if __name__ == "__main__":
     import sys
-    app = QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)   
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
